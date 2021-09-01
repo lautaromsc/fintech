cf1081e28aa494e5369fa530f15c3460da7df3d3
lautaro <lautaromsc@gmail.com>
2021-09-01 17:39:04 -0300

    se cambia index messsage

--- backend/src/controllers/index.controller.ts
+++ backend/src/controllers/index.controller.ts
@@ -6,7 +6,7 @@ class IndexController {
 
     public index (req: Request, res: Response){
         //res.json({text: 'the API is /api/users'})
-        res.sendFile(path.join(__dirname, ''))
+        res.sendFile(path.join(__dirname, 'Estoy vivo!'))
     }
 
 }
