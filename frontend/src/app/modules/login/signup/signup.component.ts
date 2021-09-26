bb74b4460489de8ce4471452d5ef68cdd8e4d425
Lautaro Mascitti <Lautaro.Mascitti@ext.prosegur.com>
2021-09-26 00:53:14 -0300

    se agrega registro de usuario

--- frontend/src/app/modules/login/signup/signup.component.ts
+++ frontend/src/app/modules/login/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import md5 from 'md5'
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  public hide = true;
+  public loginForm: FormGroup;
+  public loading: boolean;
+  public error: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private AuthenticationService: AuthenticationService,
+    private router: Router
+  ) { }
+
+
+
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    })
   }
+  
+  get form() { return this.loginForm.controls; }
+
+
+  public login = () => {
+    
+    this.AuthenticationService.login(this.form.username.value, md5(this.form.password.value) )
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['home/tp1']);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
+  }
+
 
 }
