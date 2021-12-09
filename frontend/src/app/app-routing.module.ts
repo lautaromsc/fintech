b66346bffb3fa931f35224409c6b3074a6e969c2
Lautaro Mascitti <Lautaro.Mascitti@ext.prosegur.com>
2021-12-08 22:02:01 -0300

    se crea landing page module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
  { path: '', redirectTo: '/home/shipping', pathMatch: 'full'},
  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+ { path: 'landing-page', loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule) },
  { path: "**", component: NotFound404Component }
 
 ];
