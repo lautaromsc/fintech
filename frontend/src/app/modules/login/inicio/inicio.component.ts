5b33a4bf69562d9842d72a47431754c3c1d7c08a
Lautaro Mascitti <Lautaro.Mascitti@ext.prosegur.com>
2021-12-12 16:22:40 -0300

    Se agrega inicio

--- frontend/src/app/modules/login/inicio/inicio.component.ts
+++ frontend/src/app/modules/login/inicio/inicio.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthenticationService } from 'src/app/services/auth/authentication.service';
+import md5 from 'md5'
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +13,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+
+  public hide = true;
+  public loginForm: FormGroup;
+  public loading: boolean;
+  public error: boolean;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private AuthenticationService: AuthenticationService,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) { }
+
+
+
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    })
+  }
+  
+  get form() { return this.loginForm.controls; }
+
+
+  public login = () => {
+    this.error = false
+    this.AuthenticationService.login(
+      this.form.username.value, 
+      md5(this.form.password.value) 
+    )
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['home/tp4/transferencias']);
+          localStorage.setItem('userID', data.body)
+        },
+        error => {
+          this.error = true;
+          this.loading = false;
+          //this.openSnackBar()
+        });
   }
 
+
+  openSnackBar() {
+    this._snackBar.open('Username or Password incorrect!', 'Close', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
 }
